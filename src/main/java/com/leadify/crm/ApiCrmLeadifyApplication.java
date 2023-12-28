a43f218aec44cf3b4205f123568d11677cdb0d3d
Alex <m.alejandro.quintero@gmail.com>
2023-12-27 19:04:28 -0600

    first commit

---
+++ src/main/java/com/leadify/crm/ApiCrmLeadifyApplication.java
@@ -0,0 +1,13 @@
+package com.leadify.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrmLeadifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrmLeadifyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/leadify/crm/entity/Intersted.java
@@ -0,0 +1,45 @@
+package com.leadify.crm.entity;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "build")
+@Table(name = "Intersted")
+public class Intersted {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String first_name;
+    @Column(nullable = true)
+    private String second_name;
+    @Column(nullable = false)
+    private String business_name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String campaign;
+    @Column(nullable = false)
+    private LocalDateTime interest_date;
+    @Column(nullable = false)
+    private Boolean booked;
+    @Column(nullable = false)
+    private String stage;
+    @Column(nullable = true)
+    private String notes;
+    @CreationTimestamp
+    private LocalDateTime created;
+    @UpdateTimestamp
+    private LocalDateTime updated;
+}
